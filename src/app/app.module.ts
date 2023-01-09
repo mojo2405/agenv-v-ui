3f0cbcbf96376d61fae0f0feabc2f6c6878a2cb7
Eitan Shaked <eitansh@nvidia.com>
2023-01-09 11:51:49 +0200

    fixed imports and added tabpanel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table'
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import { AptDataModule } from './components/apt-data/apt-data.module';
+import {MatIconModule} from '@angular/material/icon';
+import { SearchModule } from './components/search/search.module';
 import { HttpClientModule } from '@angular/common/http';
 import { GoogleApiService } from './services/googleapi.service';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatFormFieldModule} from '@angular/material/form-field'
+import { MainTableModule } from './components/tables/main-table.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TokenStorageService } from './services/toker-storage.service';
+import { RouterModule } from '@angular/router';
+import { MainAppModule } from './components/main-app/main-app.module';
+import { LoginComponent } from './components/login/login.component';
 import { LoginModule } from './components/login/login.module';
 import { AuthGuard } from './services/authGuard.service';
 import { MatInputModule } from '@angular/material/input';
